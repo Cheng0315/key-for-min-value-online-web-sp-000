68c78ac98513a5250f6c351a1bdd742e9ea131b1
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 10:07:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_value_key = ""
 
   name_hash.each do |key, value|
-    if smallest_value_key == "" || value < smallest_value_key
+    if smallest_value_key == "" || value < name_hash[smallest_value_key]
       smallest_value_key = key
     end
   end
